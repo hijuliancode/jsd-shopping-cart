bed95cde0d41d5ef0fd230b5600a1212aa46c00b
JulianCode <hijuliansosa@protonmail.com>
2020-05-18 12:25:04 -0500

    Adding the selected courses to the cart

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //// Variables
 const carrito = document.querySelector('#carrito')
 const cursos = document.querySelector('#lista-cursos')
+const listaCursos = document.querySelector('#lista-carrito tbody')
 
 //// Listeners
 loadEventListeners()
@@ -23,5 +24,29 @@ function comprarCurso(e) {
 
 // lee los datos del curso
 function leerDatosCurso(curso) {
-  console.log(curso)
+  const infoCurso = {
+    imagen: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent,
+    id: curso.querySelector('a').getAttribute('data-id'),
+  }
+  insertarCarrito(infoCurso)
+}
+
+// Muestra el curso seleccionado en el carrito
+function insertarCarrito(curso) {
+  const row = document.createElement('tr')
+  const {imagen, titulo, precio, id} = curso;
+
+  row.innerHTML = `
+    <td>
+      <img src="${imagen}" width="100"/>
+    </td>
+    <td>${titulo}</td
+    <td>${precio}</td>
+    <td>
+      <a href="#" class="borrar-curso" data-id="${id}">X</a>
+    </td>
+  `;
+  listaCursos.appendChild(row);
 }
